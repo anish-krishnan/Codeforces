246bb78c237af24e42e1af4e0913eaee456cfad5
Anish Krishnan <anishtech1@gmail.com>
2020-06-10 16:13:30 -0700

    Finished Magnets problem

--- 1344/B/magnets.cpp
+++ 1344/B/magnets.cpp
@@ -6,19 +6,29 @@
   *       We can verify this by checking for 
   *       [black] ... [white ... white] ... [black]
   *   
-  *   2) Every row & col contains atleast 1 black square
+  *   2) Every row & col contains atleast 1 black square 
+  *       OR we are able to place South magnets on white squares
   * 
   *   3) Return numIslands where an island is region of black squares
   *  
-  * Runtime: O(n)
+  * Runtime: O(n^2)
  */
  
 #include <iostream>
 #include <vector>
+#include <unordered_set>
  
 using namespace std;
 #define fio ios_base::sync_with_stdio(true); cin.tie(NULL); cout.tie(NULL);
  
+void print_set(unordered_set <int> const &s){
+  cout << "{";
+  for (auto const& i : s){
+    cout << i << " ";
+  }
+  cout << "}\n";
+}
+
 void print_grid(vector<vector<int>> grid){
   int n = grid.size();
   int m = grid[0].size();
@@ -62,33 +72,28 @@ bool check_convexity(vector<vector<int>> &grid){
   }
   return true;
 }
- 
-bool check_row_col(vector<vector<int>> &grid){
+
+
+bool check_row_col(vector<vector<int>> &grid, unordered_set<int> &black_rows, unordered_set<int> &black_cols){
   int n = grid.size();
   int m = grid[0].size();
- 
-  bool saw_black;
- 
+  
+  unordered_set <int> filled_white_rows;
+  unordered_set <int> filled_white_cols;
+      
   for(int i=0; i<n; i++){
-    saw_black = false;
     for(int j=0; j<m; j++){
-      if(grid[i][j] == 0)
-        saw_black = true;
+      if(grid[i][j] == 1 && black_rows.find(i) == black_rows.end() && 
+            black_cols.find(j) == black_cols.end()){
+          filled_white_rows.insert(i);
+          filled_white_cols.insert(j);
+      }
     }
-    if(!saw_black)
-      return false;
   }
- 
-  for(int j=0; j<m; j++){
-    saw_black = false;
-    for(int i=0; i<n; i++){
-      if(grid[i][j] == 0)
-        saw_black = true;
-    }
-    if(!saw_black)
-      return false;
-  }
-  return true;
+  
+  int total_rows = black_rows.size() + filled_white_rows.size();
+  int total_cols = black_cols.size() + filled_white_cols.size();
+  return total_rows == n && total_cols == m;
 }
  
  
@@ -116,7 +121,7 @@ int num_islands(vector<vector<int>> &grid){
   vector<vector<int>> visited(
     n,
     vector<int>(m, 0));
- 
+
   int count = 0;
  
   for(int i=0; i<n; i++){
@@ -133,12 +138,14 @@ int main(){
   int n, m;
   char c;
   cin >> n >> m;
- 
-  bool all_white = true;
+  
  
   vector<vector<int>> grid(
     n,
     vector<int>(m, -1));
+  
+  unordered_set <int> blackRows; 
+  unordered_set <int> blackCols; 
  
   for(int i=0; i<n; i++){
     for(int j=0; j<m; j++){
@@ -147,24 +154,21 @@ int main(){
         grid[i][j] = 1;
       } else if(c == '#'){
         grid[i][j] = 0;
-        all_white = false;
+        blackRows.insert(i);
+        blackCols.insert(j);
       }
     }
   }
-  
-  // print_grid(grid);
- 
+
   bool is_convex = check_convexity(grid);
-  bool is_row_col = check_row_col(grid);
+  bool is_row_col = check_row_col(grid, blackRows, blackCols);
  
-  // cout << "convexity: " << check_convexity(grid) << endl;
-  // cout << "row/col: " << check_row_col(grid) << endl;
-  // cout << "num_islands: " << num_islands(grid) << endl;
- 
-  if(all_white || (is_convex && is_row_col))
+  if(is_convex && is_row_col)
     cout << num_islands(grid) << endl;
   else
     cout << "-1" << endl;
  
   return 0;
 }
+
+
